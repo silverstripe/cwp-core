8153aec78ebb0119b30113f85f17d877853a55df
Kirk Mayo <kirk@silverstripe.com>
2014-08-06 14:23:41 +1200

    NEW: Adding a class to generate Atom feeds

---
+++ code/CwpAtomFeed.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * CwpAtomFeed class
+ *
+ * This class is used to create an Atom feed.
+ * @todo Improve documentation
+ * @package cwp-core
+ */
+class CwpAtomFeed extends RSSFeed {
+
+	public function __construct(SS_List $entries, $link, $title,
+			$description = null, $titleField = "Title",
+			$descriptionField = "Content", $authorField = null,
+			$lastModified = null, $etag = null) {
+		parent::__construct($entries, $link, $title, $description, $titleField,
+			$descriptionField, $authorField, $lastModified);
+		$this->setTemplate('AtomFeed');
+	}
+
+	/**
+	 * Include an link to the feed
+	 *
+	 * @param string $url URL of the feed
+	 * @param string $title Title to show
+	 */
+	public static function linkToFeed($url, $title = null) {
+		$title = Convert::raw2xml($title);
+		Requirements::insertHeadTags(
+			'<link rel="alternate" type="application/atom+xml" title="' . $title .
+			'" href="' . $url . '" />');
+	}
+
+	/**
+	 * Output the feed to the browser
+	 */
+	public function outputToBrowser() {
+		$output = parent::outputToBrowser();
+		$response = Controller::curr()->getResponse();
+		$response->addHeader("Content-Type", "application/atom+xml");
+		return $output;
+	}
+}
+
