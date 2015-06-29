11d55ab7919fefbefcb2c3cd7f42962796288bc6
Damian Mooyman <damian@silverstripe.com>
2015-06-30 11:36:51 +1200

    Add hook to boost extension

--- code/model/SolrSearchIndex.php
+++ code/model/SolrSearchIndex.php
@@ -5,6 +5,11 @@ class SolrSearchIndex extends SolrIndex {
 		$this->addClass('SiteTree');
 		$this->addAllFulltextFields();
 		$this->addFilterField('ShowInSearch');
+		
+		// Add optional boost
+		if(SiteTree::has_extension('CwpSearchBoostExtension')) {
+			$this->setFieldBoosting('SiteTree_SearchBoost', SiteTree::config()->search_boost);
+		}
 	}
 
 }
