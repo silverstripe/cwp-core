4a8a4b7c566922983331f2b285700d29130f1b65
Robbie Averill <robbie@silverstripe.com>
2019-06-19 11:33:38 +1200

    NEW Add PBKDF2 PasswordEncryptor with SHA-512 as the default algorithm for NZISM compliance
    This also updates the core constraints to SilverStripe 4.5, which requires PHP 7.1, and
    updates the Travis build matrix to match

---
+++ src/PasswordEncryptor/PBKDF2.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace CWP\Core\PasswordEncryptor;
+
+use Exception;
+use SilverStripe\Security\PasswordEncryptor_PHPHash;
+
+/**
+ * Provides Password-Based Key Derivation Function hashing for passwords, using the provided algorithm (default
+ * is SHA512), which is NZISM compliant under version 3.2 section 17.2.
+ */
+class PBKDF2 extends PasswordEncryptor_PHPHash
+{
+    /**
+     * The number of internal iterations for hash_pbkdf2() to perform for the derivation.
+     *
+     * @var int
+     */
+    protected $iterations = 10000;
+
+    /**
+     * @param string $algorithm
+     * @param int|null $iterations
+     * @throws Exception If the provided algorithm is not available in the current environment
+     */
+    public function __construct(string $algorithm, int $iterations = null)
+    {
+        parent::__construct($algorithm);
+
+        if ($iterations !== null) {
+            $this->iterations = $iterations;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getIterations(): int
+    {
+        return $this->iterations;
+    }
+
+    public function encrypt($password, $salt = null, $member = null)
+    {
+        return hash_pbkdf2($this->getAlgorithm(), $password, $salt, $this->getIterations());
+    }
+}
